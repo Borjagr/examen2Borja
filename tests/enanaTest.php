d89c97be44f61534708b77eef0fa35786223df13
Borja <borjagr97@gmail.com>
2024-02-06 12:22:01 +0100

    test herida grave

--- tests/enanaTest.php
+++ tests/enanaTest.php
@@ -35,7 +35,10 @@ class EnanaTest extends TestCase {
     public function testHeridaGrave() {
         #Se probará el efecto de una herida grave a una Enana con una situación de viva.
         #Se tendrá que probar que la vida es 0 y además que su situación es limbo
-
+        $enana = new Enana("Borja", 20);
+        $enana->heridaGrave();
+        $this->assertEquals(0, $enana->getPuntosVida());
+        $this->assertEquals("limbo", $enana->getSituacion());
     }
     
     public function testPocimaRevive() {
